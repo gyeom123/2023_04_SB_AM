dff3497c0b3b4da7000a5cdde7f206c91309588b
gyemo123 <gyeomju6536@gmail.com>
2023-04-21 14:56:38 +0900

    jsp connect , main

--- src/main/java/com/koreaIT/demo/controller/UsrHomeController.java
+++ src/main/java/com/koreaIT/demo/controller/UsrHomeController.java
@@ -8,9 +8,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class UsrHomeController {
 	
 	@RequestMapping("/usr/home/main")
-	@ResponseBody
 	public String showMain() {
-		return "안녕하세요1";
+		return "usr/home/main";
+	}
+	
+	@RequestMapping("/")
+	public String showRoot() {
+		return "redirect:/usr/home/main"; 
+		//redirect: => 요청 , 내가 받아온 요청을 다시 돌려보내는 역할
 	}
 	
 }
