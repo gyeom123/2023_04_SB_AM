bfda646e42b12f04761fb3c28f94b18199c5ac91
gyemo123 <gyeomju6536@gmail.com>
2023-04-25 15:54:13 +0900

    interceptor 구현 중

--- src/main/java/com/koreaIT/demo/interceptor/NeedLoginInterceptor.java
+++ src/main/java/com/koreaIT/demo/interceptor/NeedLoginInterceptor.java
@@ -1,4 +1,4 @@
-package com.koreaIT.demo.interceptor.copy;
+package com.koreaIT.demo.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -6,12 +6,21 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import com.koreaIT.demo.vo.Rq;
+
 @Component
-public class BeforeActionInterceptor implements HandlerInterceptor {
+public class NeedLoginInterceptor implements HandlerInterceptor {
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		
+		Rq rq = (Rq) request.getAttribute("Rq");
+		
+		if (rq.getLoginedMemberId() == 0) {
+			rq.jsPrintHistoryBack("로그인 후 이용해주세요");
+			return false;
+		}
 		
 		return HandlerInterceptor.super.preHandle(request, response, handler);
 	}
